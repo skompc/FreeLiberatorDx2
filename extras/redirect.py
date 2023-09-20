7af2390bc1865d8a63fe14c0287362c555e51420
SquirrelDev <natesnerds@outlook.com>
2023-09-20 06:19:53 -0500

    .

--- extras/redirect.py
+++ extras/redirect.py
@@ -4,27 +4,25 @@ from mitmproxy import http
 def request(flow: http.HTTPFlow) -> None:
 
     #Webview
-    #if flow.request.pretty_host == "d2-megaten-l.sega.jp":
-    #    flow.request.host = "localhost"
-    #    flow.request.port = 3000
-    #    flow.request.scheme = 'http'
+    if flow.request.pretty_host == "d2-megaten-l.sega.jp":
+        flow.request.host = "localhost"
+        flow.request.port = 3000
+        flow.request.scheme = 'http'
 
     #Assets
-    #if flow.request.pretty_host == "d2r-dl.d2megaten.com":
-    #    flow.request.host = "localhost"
-    #    flow.request.port = 3000
-    #    flow.request.scheme = 'http'
+    if flow.request.pretty_host == "d2r-dl.d2megaten.com":
+        flow.request.host = "localhost"
+        flow.request.port = 3000
+        flow.request.scheme = 'http'
 
     #Game
-    #if flow.request.pretty_host == "d2r-sim.d2megaten.com":
-    #    #flow.request.url = flow.request.url.replace("socialsv", "api/socialsv")
-    #    flow.request.host = "localhost"
-    #    flow.request.port = 8000
-    #    flow.request.scheme = 'http'
+    if flow.request.pretty_host == "d2r-sim.d2megaten.com":
+        flow.request.host = "localhost"
+        flow.request.port = 8000
+        flow.request.scheme = 'http'
 
     #Chat
-    #if flow.request.pretty_host == "d2r-chat.d2megaten.com":
-    #    flow.request.host = "localhost"
-    #    flow.request.port = 8000
-    #    flow.request.scheme = 'http'
-    #    #flow.request.url = flow.request.url.replace("socialsv", "api/socialsv")
+    if flow.request.pretty_host == "d2r-chat.d2megaten.com":
+        flow.request.host = "localhost"
+        flow.request.port = 8000
+        flow.request.scheme = 'http'
