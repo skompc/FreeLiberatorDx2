9fe16332b7927150d8ec0bbfb6c82be9206f2c0d
SquirrelDev <natesnerds@outlook.com>
2023-09-20 06:12:35 -0500

    fix TutorialBattleNext.js

--- socialsv/TutorialBattleNext.js
+++ socialsv/TutorialBattleNext.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
-const paramTools = require("../tools/paramTools")
+const decrypt = require("../tools/decrypt")
 
 function TutorialBattleNext(req, res) {
-    var input = JSON.stringify(req.query);
-    var params = JSON.parse(paramTools.clean(input,0,0));
-    var data2Get = params["wave"];
-    let file = fs.readFileSync("./json/battles/tutorial/" + data2Get +".json", "utf8");
+    let params = decrypt.stringToJsonObject(decrypt.decrypt(req.query.param));
+
+    var wave = params.wave;
+    let file = fs.readFileSync("./json/battles/tutorial/" + wave +".json", "utf8");
     let data = JSON.parse(file);
     res.status(200).json(data);
 }
