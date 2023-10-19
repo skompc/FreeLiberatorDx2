af5278bd30451c66b03205c7289a78f74a09aafb
SquirrelDev <natesnerds@outlook.com>
2023-10-19 11:35:00 -0500

    Remove Dramas (for now), add delay time to json scraper to avoid server crashes

--- main/scraper.js
+++ main/scraper.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 const axios = require('axios');
 const path = require('path');
 
-const check_code = "6.2.20.H5P8MSGzwCQM" // Obtained from legit app request
+const check_code = "6.3.00.GdSggoER4hFp" // Obtained from legit app request
 const lang_code = "en" // Language to download
-const lang_num = 1 // I don't know if this matters or not, but it's 1 for English
+const lang_num = 1 // 1 for English
 const platform = "Android" // Android for now... I don't know the correct url for apple devices.
 const platform_num = 2 // 2 for Android, idk what it would be for iOS
 
