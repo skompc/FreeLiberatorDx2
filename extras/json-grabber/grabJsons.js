9149081d7d2cd2c9ec31d6103c7a45ca5fc30ff9
SquirrelDev <natesnerds@outlook.com>
2023-11-09 16:25:01 -0600

    .

--- extras/json-grabber/grabJsons.js
+++ extras/json-grabber/grabJsons.js
@@ -29,14 +29,14 @@ const cookieValue = `JSESSIONID=${sid}`;
 // Set the cookie in the request headers
 client.defaults.headers.common['Cookie'] = cookieValue;
 
-// This is a function to add random wait times to the scraper
+// This is a function to add wait times to the scraper
 function wait(ms) {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
 }
 
-// A retry function to retry any failed downloads (will update later to be the full download function)
+// A download function
 function download(url, fileName) {
   client.get(url)
     .then(response => {
@@ -56,6 +56,8 @@ function download(url, fileName) {
 
 // Fetch List-Making URLs
 function getLists() {
+
+  // Gotta make sure this function finishes BEFORE the caller's next line is run
   return new Promise((resolve) => {
     // For Quest List
     let dec = `_tm_=14`;
@@ -65,17 +67,24 @@ function getLists() {
 
     // For Drama Lists
     for (let i = 1; i < 9; i++) {
+
       let dec1 = `id=${i}&_tm_=55`;
       let enc1 = encrypt(dec1, ek);
       let url1 = `https://d2r-sim.d2megaten.com/socialsv/ReadBack.do?param=${enc1}`;
       download(url1, `./${i}.json`)
+
     }
   });
 }
 
 // Main function obviously
 async function main() {
+
+  // Grab the lists
   await getLists();
+
+  makeDramaList();
+  makeQuestList();
 
   // Quest ids
   let questArray = JSON.parse(fs.readFileSync("./quests.json")).quests;
@@ -86,8 +95,9 @@ async function main() {
   // Init empty array
   let urlArray = [];
 
-  //  Make Drama urls
+  //  Make Drama URLs
   for (const drama of dramaArray) {
+
     const dramaToGet = encodeURIComponent(drama);
     const encodedUrl = dramaToGet.replace(/\//g, '%2f');
     const dec = `lang=1&path=${encodedUrl}&_tm_=8`;
@@ -95,10 +105,12 @@ async function main() {
     const url = `https://d2r-sim.d2megaten.com/socialsv/Drama.do?param=${enc}`;
     urlArray.push(url);
     console.log(`pushed: ${url}`);
+
   }
 
-  // Fetch Drama urls
+  // Fetch Drama URLs
   for (let i = 0; i < dramaArray.length; i++) {
+
     let url = urlArray[i];
     let fileName = `../json/dramas/${dramaArray[i]}.json`;
 
@@ -118,23 +130,86 @@ async function main() {
   // Reinit urlArray
   urlArray = [];
 
-  // Make Battle urls
+  // Make BattleEntry URLs
   for (const quest of questArray) {
 
-    // BattleEntry
     const dec0 = `stage=${quest}&main_smn=1&sub_smn=0&main_idx=1&sub_idx=0&helper=0&smn_id=0&is_auto=0&_tm_=108`;
     const enc0 = encrypt(dec0, ek);
     const url0 = `https://d2r-sim.d2megaten.com/socialsv/BattleEntry.do?param=${enc0}`;
     urlArray.push(url0);
 
-    // BattleNext
-    const dec1 = `stage=${quest}&wave=1&an_info=%5b%7b%22id%22%3a+11910%2c%22attr%22%3a+127%7d%5d&item_use=&df_info=%5b%7b%22uniq%22%3a+4%2c%22type%22%3a+2%7d%5d&turn=1&p_act=0&e_act=0&_tm_=136`;
-    const enc1 = encrypt(dec1, ek);
-    const url1 = `https://d2r-sim.d2megaten.com/socialsv/BattleNext.do?param=${enc1}`;
-    urlArray.push(url1);
+  }
+
+  // Fetch BattleEntry URLs
+  for (let i = 0; i < questArray.length; i++) {
+
+    let url = urlArray[i];
+    let fileName = `../json/battles/story/${questArray[i]}/0.json`;
+
+    fs.mkdir(`../json/dramas/${questArray[i]}/..`, { recursive: true }, (err) => {
+      if (err) {
+        console.error(`Error creating directory: ${err.message}`);
+      } else {
+        console.log(`Created directory for: ${questArray[i]}`);
+      }
+    });
+
+    download(url, fileName);
+
+    await wait(5000);
+  }
+
+  // Reinit urlArray
+  urlArray = [];
+
+  // Make BattleNext URLs
+  for (let i = 0; i < questArray.length; i++) {
+
+    urlArray.push(questArray[i])
+    let wave_max = JSON.parse(fs.readFileSync(`../json/battles/story/${questArray[i]}/0.json`)).wave_max;
+
+    for (let wave = 1; wave < wave_max; wave++) {
+      const dec1 = `stage=${quest}&wave=${wave}&an_info=%5b%7b%22id%22%3a+11910%2c%22attr%22%3a+127%7d%5d&item_use=&df_info=%5b%7b%22uniq%22%3a+4%2c%22type%22%3a+2%7d%5d&turn=1&p_act=0&e_act=0&_tm_=136`;
+      const enc1 = encrypt(dec1, ek);
+      const url1 = `https://d2r-sim.d2megaten.com/socialsv/BattleNext.do?param=${enc1}`;
+      urlArray.push(url1);
+    }
+
+    urlArray.push("SPLIT")
 
   }
 
+  // Split BattleNext URLs into multiple arrays
+  let splitArrays = [];
+  let currentArray = [];
+
+  for (const url of urlArray) {
+    if (url === "SPLIT") {
+      // If "SPLIT" is encountered, push the currentArray to splitArrays
+      splitArrays.push(currentArray);
+      currentArray = []; // Reset the currentArray
+    } else {
+      // Otherwise, add the URL to the currentArray
+      currentArray.push(url);
+    }
+  }
+
+  if (currentArray.length > 0) {
+    splitArrays.push(currentArray);
+  }
+
+  // Fetch BattleNext URLs
+  for (let i = 0; i < splitArrays.length; i++) {
+
+    let newCurrentArray = splitArrays[i];
+    
+
+    download(url, fileName);
+
+    await wait(5000);
+  }
+
 }
+
 
 main();
